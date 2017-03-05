a5c7051dcb26f3dcccca1e1b6bfc62117aa2acdb
aaroniousblanco <aaronawhite1@gmail.com>
2017-03-05 16:54:43 -0500

    More JS practice.

--- js_highorderexercises19.js
+++ js_highorderexercises19.js
@@ -2,15 +2,24 @@
 //
 // Implement a forEach function which takes two arguments: an array arr and a function fun. It will call fun passing each item in arr to fun as the first argument. Example:
 //
-// var arr = [
-//   { name: 'Bob' },
-//   { name:'Alice' },
-//   { name:'Joe' }
-// ];
-//
-// forEach(arr, function(person) {
-//   console.log('Hello, ' + person.name + '!');
-// });
+var arr = [
+  { name: 'Bob' },
+  { name:'Alice' },
+  { name:'Joe' }
+];
+
+function forEach(array, fun) {
+  for (var i = 0; i < array.length; i++) {
+    fun(array[i]);
+  }
+}
+
+forEach(arr, function(person) {
+  console.log('Hello, ' + person.name + '!');
+});
+
+
+
 // The above program will print:
 //
 // Hello, Bob!
