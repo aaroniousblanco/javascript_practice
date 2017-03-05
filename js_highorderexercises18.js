c4ac699b1aeb094cac8d24cf4983b409f2bcb6f2
aaroniousblanco <aaronawhite1@gmail.com>
2017-03-05 17:41:07 -0500

    More JS practice.

--- js_highorderexercises18.js
+++ js_highorderexercises18.js
@@ -1,3 +1,23 @@
 // Caesar Cipher
 //
 // You guessed it! Write the cipher(secret) function that returns the ciphered string (use offset 13).
+
+function cipher(text) {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  var result = '';
+  for (var i = 0; i < text.length; i++) {
+    var chr = text[i];
+    var idx = alphabet.indexOf(chr.toUpperCase());
+    var newIdx = idx + 13;
+    if (newIdx >= alphabet.length) {
+      newIdx -= 26;
+    }
+    result += alphabet[newIdx];
+  }
+  return result;
+}
+
+// You can assume that the text is only one word, all letters are capitalized, and the offset is always 13
+var encrypted = cipher('GENIUS');
+
+console.log(encrypted);
--- js_highorderexercises20.js
+++ js_highorderexercises20.js
@@ -2,5 +2,20 @@
 //
 // Implement a map function which takes two arguments: an array arr and a function fun. It will return a new array, with each of its results being the result of calling fun with argument. Example:
 //
-// > map([1, 2, 3], function(n) { return n * n; })
-// [2, 4, 6]
+
+var arr = [1, 2, 3];
+
+function map(array, fun) {
+  newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    vari = fun(array[i]);
+    newArray.push(vari);
+  }
+  return newArray;
+}
+
+finalArray = map(arr, function(n) {
+  return n * n;
+});
+
+console.log(finalArray);
